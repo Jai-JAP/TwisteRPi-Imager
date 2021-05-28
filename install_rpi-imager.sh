f7dd73bb7f94bcd5dc4fe24112f00404b2fd30ff
Jai-JAP <78354625+Jai-JAP@users.noreply.github.com>
2021-05-28 21:41:17 +0530

    Update install_rpi-imager.sh

--- install_rpi-imager.sh
+++ install_rpi-imager.sh
@@ -7,10 +7,11 @@ if [ ! -e /usr/bin/rpi-imager ]; then
     echo "RPi-Imager not installed, installing it."
     if [ ! $ARCH == armhf ] || [ ! $ARCH == arm64 ] || [ ! $ARCH == i386 ] || [ ! $ARCH == amd64 ] ; then
         echo "Unsupported architecture ${ARCH}"
+        exit 1
     fi
-    wget -O /tmp/rpi-imager.deb http://archive.raspberrypi.org/debian/pool/main/r/rpi-imager/rpi-imager_${VER}_${ARCH}.deb
-    apt-get install /tmp/rpi-imager.deb
-    rm /tmp/rpi-imager.deb
+    sudo wget -O /tmp/rpi-imager.deb http://archive.raspberrypi.org/debian/pool/main/r/rpi-imager/rpi-imager_${VER}_${ARCH}.deb
+    sudo apt-get install /tmp/rpi-imager.deb
+    sudo rm /tmp/rpi-imager.deb
     exit 0
 else
     echo "RPi-Imager is installed, continuing..."
